a29a9a113b373a7befa182ff1f38298a67f47e79
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-10-11 20:21:15 +0200

    Readded itemBlockMachine bits for tooltips and unlocalized name

--- src/main/java/cofh/thermalexpansion/block/machine/ItemBlockMachine.java
+++ src/main/java/cofh/thermalexpansion/block/machine/ItemBlockMachine.java
@@ -2,14 +2,16 @@ package cofh.thermalexpansion.block.machine;
 
 import cofh.api.tileentity.IRedstoneControl.ControlMode;
 import cofh.core.item.ItemBlockCoFHBase;
-import cofh.lib.util.helpers.AugmentHelper;
-import cofh.lib.util.helpers.EnergyHelper;
-import cofh.lib.util.helpers.ItemHelper;
-import cofh.lib.util.helpers.RedstoneControlHelper;
+import cofh.lib.util.helpers.*;
 import cofh.thermalexpansion.util.ReconfigurableHelper;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.translation.I18n;
+
+import java.util.List;
 
 public class ItemBlockMachine extends ItemBlockCoFHBase {
 
@@ -24,7 +26,7 @@ public class ItemBlockMachine extends ItemBlockCoFHBase {
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 
-		return "tile.thermalexpansion.machine." + BlockMachine.Type.byMetadata(ItemHelper.getItemDamage(stack)).getName() + ".name";
+		return "tile.thermalexpansion.machine." + BlockMachine.Type.byMetadata(ItemHelper.getItemDamage(stack)).getName();
 	}
 
 	/* HELPERS */
@@ -51,6 +53,49 @@ public class ItemBlockMachine extends ItemBlockCoFHBase {
 			setDefaultTag(container);
 		}
 		return container.getTagCompound().getByte("Level");
+	}
+
+	@Override
+	public String getItemStackDisplayName(ItemStack stack) {
+
+		String unloc = getUnlocalizedNameInefficiently(stack);
+		String unloc2 = '.' + NAMES[getLevel(stack)];
+
+		if (I18n.canTranslate(unloc + unloc2 + ".name")) {
+			return StringHelper.localize(unloc + unloc2 + ".name");
+		}
+
+		return StringHelper.localize(unloc + ".name") + " (" + StringHelper.localize("info.thermalexpansion" + unloc2) + ")";
+	}
+
+	@Override
+	public EnumRarity getRarity(ItemStack stack) {
+
+		switch (getLevel(stack)) {
+			case 3:
+				return EnumRarity.RARE;
+			case 2:
+				return EnumRarity.UNCOMMON;
+			default:
+				return EnumRarity.COMMON;
+		}
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+
+		SecurityHelper.addOwnerInformation(stack, tooltip);
+		if (StringHelper.displayShiftForDetail && !StringHelper.isShiftKeyDown()) {
+			tooltip.add(StringHelper.shiftForDetails());
+		}
+		if (!StringHelper.isShiftKeyDown()) {
+			return;
+		}
+		SecurityHelper.addAccessInformation(stack, tooltip);
+
+		tooltip.add(StringHelper.getInfoText("info.thermalexpansion.machine." + BlockMachine.Type.byMetadata(ItemHelper.getItemDamage(stack)).getName()));
+
+		RedstoneControlHelper.addRSControlInformation(stack, tooltip);
 	}
 
 	public static final String[] NAMES = { "basic", "hardened", "reinforced", "resonant" };
